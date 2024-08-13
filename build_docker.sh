432d7f1f0c11ad329ac0d11fd4f524e98c9ca565
a-parida12 <abhijeet.parida@tum.de>
2024-08-13 14:11:00 -0400

    singularity working

--- build_docker.sh
+++ build_docker.sh
@@ -1,4 +1,4 @@
  source ../ISBI2024-BraTS-GOAT/.mlcubes/bin/activate
 cd segmenter/mlcube
 mlcube configure -Pdocker.build_strategy=always
-docker push aparida12/brats-peds-2023:ped-v2
+docker push aparida12/brats-peds-2023:ped
