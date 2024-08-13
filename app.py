80f0bb947d2b317971ef63090b7a1a55232f314c
a-parida12 <abhijeet.parida@tum.de>
2024-08-13 17:01:39 -0400

    updated teh app

--- app.py
+++ app.py
@@ -1,52 +1,107 @@
 import logging
-logger = logging.getLogger(__file__)
-import gradio as gr
+import subprocess
+import glob
+from pathlib import Path
 import numpy as np
 import SimpleITK as sitk
 import PIL
-from pathlib import Path
+import gradio as gr
+import os
 from app_assets import logo
-import glob
 
+logger = logging.getLogger(__file__)
+# Constants
 
-import subprocess
+# Always use dumy name to maintain annonimity 
+DUMMY_DIR = "BraTS-PED-00019-000"
+DUMMY_FILE_NAMES = {modality : f"{DUMMY_DIR}-{modality}.nii.gz" for modality in ["t1c", "t2f", "t1n", "t2w"]}
+
 
 mydict = {}
 
 def run_inference(image_t1c, image_t2f, image_t1n, image_t2w):
-    # rename all files to allow for annonimity
-    dummy_file_name_t1c = "BraTS-PED-00019-000-t1c.nii.gz"
-    dummy_file_name_t2f = "BraTS-PED-00019-000-t2f.nii.gz"
-    dummy_file_name_t1n = "BraTS-PED-00019-000-t1n.nii.gz"
-    dummy_file_name_t2w = "BraTS-PED-00019-000-t2w.nii.gz"
-    dummy_dir = dummy_file_name_t1c[:-11]
+    """Run inference on the given image paths
 
-    input_path = Path(f'/tmp/{dummy_dir}')
-    cmd = f"mkdir {input_path}; mv {image_t1c} {input_path}/{dummy_file_name_t1c}; mv {image_t2f} {input_path}/{dummy_file_name_t2f}; mv {image_t1n} {input_path}/{dummy_file_name_t1n}; mv {image_t2w} {input_path}/{dummy_file_name_t2w}"
-    print(cmd)
-    subprocess.run(cmd, shell=True)
-    cmd = f"mkdir {'./outs'}; cd cnmc_brats23/mlcube; mlcube run --gpus device=1 --task infer data_path={input_path.parent} output_path={'../outs'}"
-    print(cmd)
-    subprocess.run(cmd, shell=True)
+    Args:
+        image_paths (list): List of image paths
 
-    return input_path, f'./cnmc_brats23/mlcube/outs/{dummy_dir}.nii.gz'
+    Returns:
+        path.like, path.like: inpuit path, output path
+    """
+    image_paths = {
+        "t1c": image_t1c, 
+        "t2f": image_t2f, 
+        "t1n": image_t1n, 
+        "t2w":image_t2w}
+    input_path = Path(f'/tmp/peds-app/{DUMMY_DIR}')
+    os.makedirs(input_path, exist_ok=True)
+    output_folder = Path('./segmenter/mlcube/outs')
+    os.makedirs(output_folder, exist_ok=True)
+    output_path = output_folder / f'seg_{Path(image_t1c).name}'
+    fake_output_path = output_folder / f'{DUMMY_DIR}.nii.gz'
+    # Create directories and move files
+    
+    for key, file in image_paths.items():
+        subprocess.run(f"cp {file} {input_path}/{DUMMY_FILE_NAMES[key]}", shell=True)
+    # delete original files
+    for _, file in image_paths.items():
+        subprocess.run(f"rm  {file}", shell=True)
+
+    mlcube_cmd =f"docker run --shm-size=2gb --gpus=all -v {input_path.parent}:/input/ -v {output_folder.absolute()}:/output aparida12/brats-peds-2023:ped infer --data_path /input/ --output_path /output/"
+    #mlcube_cmd = f"cd ./segmenter/mlcube; mlcube run --gpus device=1 --task infer data_path={input_path}/ output_path=../outs"
+    print(mlcube_cmd)
+    subprocess.run(mlcube_cmd, shell=True)
+    subprocess.run(f"mv {fake_output_path} {output_path}", shell=True)
+
+    return str(input_path), str(output_path)
 
 def get_img_mask(image_path, mask_path):
+    """_summary_
+
+    Args:
+        image_path (_type_): _description_
+        mask_path (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     img_obj = sitk.ReadImage(image_path)
     mask_obj = sitk.ReadImage(mask_path)
     img = sitk.GetArrayFromImage(img_obj)
     mask = sitk.GetArrayFromImage(mask_obj)
+    
+    # Normalize image
     minval, maxval = np.min(img), np.max(img)
-    img = ((img-minval)/(maxval-minval)).clip(0,1)*255
+    img = ((img - minval) / (maxval - minval)).clip(0, 1) * 255
 
     return img, img_obj, mask
+
+def calculate_volumes(mask, spacing_tuple):
+    multiplier_ml = 0.001 * np.prod(spacing_tuple)
+    unique, frequency = np.unique(mask, return_counts=True)
+    volumes = {f'vol_lbl{lbl}': multiplier_ml * freq for lbl, freq in zip(unique, frequency)}
+    return volumes
+
+
+def render_slice(img, mask, x, view):
+    if view == 'axial':
+        slice_img, slice_mask = img[x, :, :], mask[x, :, :]
+    elif view == 'coronal':
+        slice_img, slice_mask = img[:, x, :], mask[:, x, :]
+    elif view == 'saggital':
+        slice_img, slice_mask = img[:, :, x], mask[:, :, x]
+    
+    slice_img = np.flipud(slice_img)
+    slice_mask = np.flipud(slice_mask)
+    return slice_img, slice_mask
+
 
 def main_func(image_t1c, image_t2f, image_t1n, image_t2w):
     print(image_t1c)
     global mydict
     image_path = image_t1c
     image_path, mask_path = run_inference(Path(image_t1c), Path(image_t2f),Path(image_t1n), Path(image_t2w))
-    image_path = glob.glob(str(image_path / '*.nii.gz'))
+    image_path = glob.glob(image_path+'/*.nii.gz')
     mydict['img_path'] = image_path
     mydict['mask_path'] = mask_path
     img, img_obj, mask = get_img_mask(image_path[0], mask_path)
@@ -66,59 +121,37 @@ def main_func(image_t1c, image_t2f, image_t1n, image_t2w):
 
 def main_func_example(file_obj):
     print(file_obj)
-    mask_path, output_text = main_func(file_obj)
+    mask_path, _ = main_func(file_obj)
     x, state = 10, 10
     im, another_output_text = render(x, state)
     return mask_path, im, another_output_text
 
-def render(file_to_render, x, state, view):
-    global mydict
-    suffix ={'T2 Flair':'t2f','native T1':'t1n', 'post-contrast T1-weighted':'t1c', 'T2 weighted': 't2w'}
-    print(suffix[file_to_render])
-    if len(mydict)>=4:
-        print(mydict['img'])
+def render(file_to_render, x, view):
+    suffix = {'T2 Flair': 't2f', 'native T1': 't1n', 'post-contrast T1-weighted': 't1c', 'T2 weighted': 't2w'}
+    if 'img_path' in mydict:
         get_file = [file for file in mydict['img_path'] if suffix[file_to_render] in file][0]
-        mydict['img'], _, _ =get_img_mask(get_file, get_file) 
-        if x < 0:
-            x = 0
-        if view == 'axial':
-            if x > mydict['img'].shape[0]-1:
-                x = mydict['img'].shape[0]-1
-            
-            img = mydict['img'][x,:,:]
-            mask = mydict['mask'][x,:,:]
-        if view == 'coronal':
-            if x > mydict['img'].shape[1]-1:
-                x = mydict['img'].shape[1]-1
-            
-            img = mydict['img'][:,x,:]
-            mask = mydict['mask'][:,x,:]
-        if view == 'saggital':
-            if x > mydict['img'].shape[2]-1:
-                x = mydict['img'].shape[2]-1
-            
-            img = mydict['img'][:,:,x]
-            mask = mydict['mask'][:,:,x]
-        img = np.flipud(img)
-        mask = np.flipud(mask)
-        im = PIL.Image.fromarray(img)
-        value = (im,[(mask==1,f"enhancing tumor: {mydict.get('vol_lbl1', 0):.3f} ml"),(mask==2,f"edema: {mydict.get('vol_lbl2', 0):.3f} ml"),(mask==3,f"necrosis: {mydict.get('vol_lbl3', 0):.3f} ml")])
-        zmin, zmax = 0,mydict['img'].shape[0]-1
+        img, _, mask = get_img_mask(get_file, get_file)
+        
+        x = max(0, min(x, img.shape[0 if view == 'axial' else (1 if view == 'coronal' else 2)] - 1))
+        slice_img, slice_mask = render_slice(img, mask, x, view)
+        
+        im = PIL.Image.fromarray(slice_img.astype(np.uint8))
+        annotations = [
+            (slice_mask == 1, f"enhancing tumor: {mydict.get('vol_lbl1', 0):.3f} ml"),
+            (slice_mask == 2, f"edema: {mydict.get('vol_lbl2', 0):.3f} ml"),
+            (slice_mask == 3, f"necrosis: {mydict.get('vol_lbl3', 0):.3f} ml")
+        ]
+        return im, annotations
     else:
-        im = np.zeros(10,10)
-        zmin, zmax = None, None
-        value = (im,[])
+        return np.zeros((10, 10)), []
 
-    return value#,f'z-value: {x}, (zmin: {zmin}, zmax: {zmax})'
-
-def render_axial(file_to_render, x, state):
-    return render(file_to_render, x, state, 'axial')
-def render_coronal(file_to_render, x, state):
-    return render(file_to_render, x, state, 'coronal')
-def render_saggital(file_to_render, x, state):
-    return render(file_to_render, x, state, 'saggital')
-
-
+def render_axial(file_to_render, x):
+    return render(file_to_render, x, 'axial')
+def render_coronal(file_to_render, x):
+    return render(file_to_render, x, 'coronal')
+def render_saggital(file_to_render, x):
+    return render(file_to_render, x, 'saggital')
+# Gradio UI
 with gr.Blocks() as demo:
     with gr.Column():
         gr.Markdown(
@@ -126,16 +159,12 @@ with gr.Blocks() as demo:
         # CNMC PMI Pediatric Brain Tumor Segmentation
         """)
     gr.HTML(value=f"<p style='margin-top: 1rem, margin-bottom: 1rem'> <img src='{logo.logo}' alt='Childrens National Logo' style='display: inline-block'/></p>")
+
     with gr.Row():
         image_t1c = gr.File(label="upload t1 contrast enhanced here:", file_types=["nii.gz"])
         image_t2f = gr.File(label="upload t2 flair here:", file_types=["nii.gz"])
         image_t1n = gr.File(label="upload t1 pre-contrast here:", file_types=["nii.gz"])
         image_t2w = gr.File(label="upload t2 weighted here:", file_types=["nii.gz"])
-
-    # with gr.Row():
-    #         image_t1c, image_t2f, image_t1n, image_t2w
-    #         image_folder= gr.Files(file_count="multiple", label="upload t1n, t1c, t2w and t2f files here:", file_types=["nii.gz"])
-    #         print("image_folder", image_folder)
     with gr.Row():
         with gr.Column():
              gr.Button("", render=False)
@@ -170,64 +199,63 @@ with gr.Blocks() as demo:
     with gr.Row():
         mask_file = gr.File(label="download annotation", height="vw" )
 
-    example_1 = [
+    example_1 = [[
         "/home/pmilab/Xinyang/data/BraTS2023/ASNR-MICCAI-BraTS2023-PED-Challenge-ValidationData/BraTS-PED-00210-000/BraTS-PED-00210-000-t1c.nii.gz",
         "/home/pmilab/Xinyang/data/BraTS2023/ASNR-MICCAI-BraTS2023-PED-Challenge-ValidationData/BraTS-PED-00210-000/BraTS-PED-00210-000-t2f.nii.gz",
         "/home/pmilab/Xinyang/data/BraTS2023/ASNR-MICCAI-BraTS2023-PED-Challenge-ValidationData/BraTS-PED-00210-000/BraTS-PED-00210-000-t1n.nii.gz",
         "/home/pmilab/Xinyang/data/BraTS2023/ASNR-MICCAI-BraTS2023-PED-Challenge-ValidationData/BraTS-PED-00210-000/BraTS-PED-00210-000-t2w.nii.gz",
-    ]
+    ]]
     example_2 =[
-    
+    [
         "/home/pmilab/Xinyang/data/BraTS2023/ASNR-MICCAI-BraTS2023-PED-Challenge-ValidationData/BraTS-PED-00208-000/BraTS-PED-00208-000-t1c.nii.gz",
         "/home/pmilab/Xinyang/data/BraTS2023/ASNR-MICCAI-BraTS2023-PED-Challenge-ValidationData/BraTS-PED-00208-000/BraTS-PED-00208-000-t2f.nii.gz",
         "/home/pmilab/Xinyang/data/BraTS2023/ASNR-MICCAI-BraTS2023-PED-Challenge-ValidationData/BraTS-PED-00208-000/BraTS-PED-00208-000-t1n.nii.gz",
         "/home/pmilab/Xinyang/data/BraTS2023/ASNR-MICCAI-BraTS2023-PED-Challenge-ValidationData/BraTS-PED-00208-000/BraTS-PED-00208-000-t2w.nii.gz",
-    ]
+    ]]
 
-    example_3 =[
-    
-        "/home/pmilab/Xinyang/data/BraTS2023/ASNR-MICCAI-BraTS2023-PED-Challenge-TrainingData/BraTS-PED-00019-000/BraTS-PED-00019-000-t1c.nii.gz",
-        "/home/pmilab/Xinyang/data/BraTS2023/ASNR-MICCAI-BraTS2023-PED-Challenge-TrainingData/BraTS-PED-00019-000/BraTS-PED-00019-000-t2f.nii.gz",
-        "/home/pmilab/Xinyang/data/BraTS2023/ASNR-MICCAI-BraTS2023-PED-Challenge-TrainingData/BraTS-PED-00019-000/BraTS-PED-00019-000-t1n.nii.gz",
-        "/home/pmilab/Xinyang/data/BraTS2023/ASNR-MICCAI-BraTS2023-PED-Challenge-TrainingData/BraTS-PED-00019-000/BraTS-PED-00019-000-t2w.nii.gz",
-    ]
 
-    #     example_1 = [
-    #     "/home/pmilab/Xinyang/data/BraTS2023/ASNR-MICCAI-BraTS2023-PED-Challenge-ValidationData/BraTS-PED-00210-000/",
-    # ]
-    gr.HTML(value=f"<center><font size='2'> The software provided 'as is', without any warranty or liability.  For research use only and not intended for medical diagnosis. We do not store or access any information uploaded to the platform. Model version v27062022.</font></center>")
-    gr.Examples(
-        examples=[example_1, example_2, example_3],
-        inputs=[image_t1c, image_t2f, image_t1n, image_t2w],
-        outputs=[mask_file,out_text],
-        fn=main_func,
-        cache_examples=False,
-        label="Preloaded BraTS 2023 examples"
-    )
+    gr.HTML(value=f"<center><font size='2'> The software provided 'as is', without any warranty or liability.  For research use only and not intended for medical diagnosis. We do not store or access any information uploaded to the platform. This version is v20240815</font></center>")
+    # gr.Examples(
+    #     examples=[example_1, example_2],
+    #     inputs=[image_folder],
+    #     outputs=[mask_file,out_text],
+    #     fn=main_func,
+    #     cache_examples=False,
+    #     label="Preloaded BraTS 2023 examples"
+    # )
     
     btn.click(fn=main_func, 
         inputs=[image_t1c, image_t2f, image_t1n, image_t2w], outputs=[mask_file, out_text],
     )
+    file_to_render.select(render_axial,
+        inputs=[file_to_render, state_axial],
+        outputs=[myimage_axial])
+    file_to_render.select(render_coronal,
+        inputs=[file_to_render, state_coronal],
+        outputs=[myimage_coronal])
+    file_to_render.select(render_saggital,
+        inputs=[file_to_render, state_saggital],
+        outputs=[myimage_saggital])
 
     slider_axial.change(
         render_axial,
-        inputs=[file_to_render, slider_axial, state_axial],
+        inputs=[file_to_render, slider_axial],
         outputs=[myimage_axial],
-        api_name="hohoho"
+        api_name="axial_slider"
     )
     slider_coronal.change(
         render_coronal,
-        inputs=[file_to_render, slider_coronal, state_coronal],
+        inputs=[file_to_render, slider_coronal],
         outputs=[myimage_coronal],
         api_name="hohoho"
     )
     slider_saggital.change(
         render_saggital,
-        inputs=[file_to_render, slider_saggital, state_saggital],
+        inputs=[file_to_render, slider_saggital],
         outputs=[myimage_saggital],
         api_name="hohoho"
     )
 
+
 if __name__ == "__main__":
-    #demo.queue().launch(auth=("admin@pmilab.cnmc.org", "pmilab2023"),server_name="0.0.0.0")
     demo.queue().launch(server_name="0.0.0.0")
