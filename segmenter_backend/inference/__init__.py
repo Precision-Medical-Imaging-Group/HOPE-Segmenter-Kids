0e17b26494972f3bd329455c0383a35db17aa81b
Zhifan Jiang <zhifan.jiang@gmail.com>
2024-11-05 10:40:03 -0700

    Update docstring summary

--- segmenter_backend/inference/__init__.py
+++ segmenter_backend/inference/__init__.py
@@ -1,5 +1,5 @@
 """
-### API Documentation for Inference Code
+### API Documentation for Inference Code (Segmenter Backend) for HOPE-Segmenter-Kids
 
-Welcome to the API documentation for the inference code. Navigate through the table of contents on the left to explore detailed documentation for each submodule.
+Welcome to the API documentation for the inference code, the segmenter backend for HOPE-Segmenter-Kids. Navigate through the table of contents on the left to explore detailed documentation for each submodule.
 """
